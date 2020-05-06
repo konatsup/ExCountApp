b851a4be99e5683754497162593f780449475baa
konatsup <konatsukomatsu@gmail.com>
2020-05-06 20:22:38 +0900

    feat: Add plus button

--- app/src/main/java/com/konatsup/excountapp/MainActivity.kt
+++ app/src/main/java/com/konatsup/excountapp/MainActivity.kt
@@ -2,11 +2,21 @@ package com.konatsup.excountapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var number: Int = 0
+        numberTextView.text = "0"
+
+        plusButton.setOnClickListener {
+            number += 1
+            numberTextView.text = number.toString()
+        }
+
     }
 }
